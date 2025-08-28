0372b3ec8b9fa80b22095fac9913ffe461ec53b2
MohamedAbedy <hdjdrtrrf@gmail.com>
2025-08-28 03:05:28 +0300

    add profile, edit checkout, check for cart

--- app/components/Contact/Info.vue
+++ app/components/Contact/Info.vue
@@ -14,7 +14,7 @@
                         </section>
                     </section>
                     <p class="lg:mt-10 mt-5 text-xl mb-2">Email</p>
-                    <a href="mailto:abedymedo@gmail.com?subject=Contact%20from%20Markly%20Website&body=Hello%20Mohamed,%0A%0AI%20would%20like%20to%20inquire%20about..."
+                    <a href="mailto:aaabbbcc@gmail.com?subject=Contact%20from%20Markly%20Website&body=Hello%20Mohamed,%0A%0AI%20would%20like%20to%20inquire%20about..."
                         class="w-56 h-12 flex items-center justify-center bg-[#1F1F1F] border-dashed border-[#262626] border-[5px] rounded-lg">abedymedo@gmail.com</a>
                 </section>
                 <section class="flex flex-col items-center lg:border-r-[5px] border-b-[5px] lg:border-b-0 border-dashed border-[#262626] lg:p-10 py-5">
@@ -24,7 +24,7 @@
                         </section>
                     </section>
                     <p class="lg:mt-10 mt-5 text-xl mb-2">Phone</p>
-                    <a href="tel:+201069199647" class="w-56 h-12 flex items-center justify-center bg-[#1F1F1F] border-dashed border-[#262626] border-[5px] rounded-lg">+20 10 69199647</a>
+                    <a href="tel:+201012345678" class="w-56 h-12 flex items-center justify-center bg-[#1F1F1F] border-dashed border-[#262626] border-[5px] rounded-lg">+20 10 69199647</a>
                 </section>
                 <section class="flex flex-col items-center border-dashed border-[#262626] lg:p-10 py-5">
                     <section class="rounded-full w-20 h-20 border-[5px] border-dashed border-[#262626] flex justify-center items-center">
@@ -33,7 +33,7 @@
                         </section>
                     </section>
                     <p class="lg:mt-10 mt-5 text-xl mb-2">Location</p>
-                    <a href="https://maps.app.goo.gl/XrzRxF4LF66CGgcc9" class="w-56 h-12 flex items-center justify-center bg-[#1F1F1F] border-dashed border-[#262626] border-[5px] rounded-lg">Get Location</a>
+                    <a href="https://maps.app.goo.gl/6EYNZFWFukSBpeie7" class="w-56 h-12 flex items-center justify-center bg-[#1F1F1F] border-dashed border-[#262626] border-[5px] rounded-lg">Get Location</a>
                 </section>
             </section>
         </section>

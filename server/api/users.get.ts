c57fb0b79bfa88fabdc12fcbba09613f9da47d19
MohamedAbedy <hdjdrtrrf@gmail.com>
2025-09-03 04:34:09 +0300

    add dashboard

--- server/api/users.get.ts
+++ server/api/users.get.ts
@@ -2,7 +2,7 @@ import admin from "../utils/firebaseAdmin";
 
 export default defineEventHandler(async () => {
   try {
-    const listUsers = await admin.auth().listUsers();
+    const listUsers = await admin.auth().listUsers(1000);
     return listUsers.users.map((user) => ({
       uid: user.uid,
       email: user.email,
